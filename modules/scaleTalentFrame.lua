8d737b2150451e619b6b925878b6207d7815c310
Mark W <numedain@gmail.com>
2022-10-30 14:49:55 +0100

    Added support for scaling the Talent Tree Viewer frame

--- modules/scaleTalentFrame.lua
+++ modules/scaleTalentFrame.lua
@@ -7,9 +7,20 @@ local Module = Main:NewModule('ScaleTalentFrame', 'AceHook-3.0', 'AceEvent-3.0')
 function Module:OnEnable()
     if self.blizzMoveEnabled then return end
 
+    local registerEvent = false
     if IsAddOnLoaded('Blizzard_ClassTalentUI') then
-        self:SetupHook();
+        self:SetupHook('Blizzard_ClassTalentUI');
     else
+        registerEvent = true;
+    end
+    if GetAddOnEnableState(UnitName('player'), 'TalentTreeViewer') == 2 then
+        if IsAddOnLoaded('TalentTreeViewer') then
+            self:SetupHook('TalentTreeViewer');
+        else
+            registerEvent = true;
+        end
+    end
+    if registerEvent then
         self:RegisterEvent('ADDON_LOADED');
     end
 end
@@ -17,7 +28,8 @@ end
 function Module:OnDisable()
     if self.blizzMoveEnabled then return end
 
-    ClassTalentFrame:SetScale(1);
+    if ClassTalentFrame then ClassTalentFrame:SetScale(1); end
+    if TalentViewer_DF then TalentViewer_DF:SetScale(1); end
     self:UnhookAll();
 end
 
@@ -46,24 +58,35 @@ function Module:GetOptions(defaultOptionsTable, db)
 end
 
 function Module:ADDON_LOADED(_, addon)
+    if addon == 'Blizzard_ClassTalentUI' or addon == 'TalentTreeViewer' then
+        self:SetupHook(addon);
+    end
+end
+
+function Module:SetupHook(addon)
     if addon == 'Blizzard_ClassTalentUI' then
-        self:SetupHook();
-        self:UnregisterEvent('ADDON_LOADED');
+        if self.db.scale == nil then
+            self.db.scale = ClassTalentFrame:GetScale();
+        end
+
+        self:HookScript(ClassTalentFrame, 'OnMouseWheel', 'OnMouseWheelBlizzard');
+        self:HookScript(ClassTalentFrame.TalentsTab.ButtonsParent, 'OnMouseWheel', 'OnMouseWheelBlizzard');
+
+        ClassTalentFrame:SetScale(self.db.scale);
+    end
+    if addon == 'TalentTreeViewer' then
+        if self.db.viewerScale == nil then
+            self.db.viewerScale = TalentViewer_DF:GetScale();
+        end
+
+        self:HookScript(TalentViewer_DF, 'OnMouseWheel', 'OnMouseWheelTalentTreeViewer');
+        self:HookScript(TalentViewer_DF.Talents.ButtonsParent, 'OnMouseWheel', 'OnMouseWheelTalentTreeViewer');
+
+        TalentViewer_DF:SetScale(self.db.viewerScale);
     end
 end
 
-function Module:SetupHook()
-    if self.db.scale == nil then
-        self.db.scale = ClassTalentFrame:GetScale();
-    end
-
-    self:HookScript(ClassTalentFrame, 'OnMouseWheel', 'OnMouseWheel');
-    self:HookScript(ClassTalentFrame.TalentsTab.ButtonsParent, 'OnMouseWheel', 'OnMouseWheel');
-
-    ClassTalentFrame:SetScale(self.db.scale);
-end
-
-function Module:OnMouseWheel(_, delta)
+function Module:OnMouseWheelBlizzard(_, delta)
     if not IsControlKeyDown() then return end
 
     local scale = self.db.scale or 1;
@@ -71,4 +94,14 @@ function Module:OnMouseWheel(_, delta)
     scale = math.max(0.5, math.min(2, scale));
     self.db.scale = scale;
     ClassTalentFrame:SetScale(scale);
+end
+
+function Module:OnMouseWheelTalentTreeViewer(_, delta)
+    if not IsControlKeyDown() then return end
+
+    local scale = self.db.viewerScale or 1;
+    scale = scale + delta * 0.05;
+    scale = math.max(0.5, math.min(2, scale));
+    self.db.viewerScale = scale;
+    TalentViewer_DF:SetScale(scale);
 end
