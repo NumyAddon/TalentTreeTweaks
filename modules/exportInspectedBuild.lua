07303993ddd67665e515c01e9b8d780ce84f64d8
Mark W <numedain@gmail.com>
2023-04-19 20:07:11 +0200

    Add button to post talent string (or link, in 10.1) to chat

--- modules/exportInspectedBuild.lua
+++ modules/exportInspectedBuild.lua
@@ -4,6 +4,8 @@ local Main = TTT.Main;
 --- @type TalentTreeTweaks_Util
 local Util = TTT.Util;
 
+local LEVEL_CAP = 70;
+
 local Module = Main:NewModule('ExportInspectedBuild', 'AceHook-3.0');
 
 function Module:OnEnable()
@@ -17,19 +19,112 @@ function Module:OnDisable()
     if ClassTalentFrame and ClassTalentFrame.TalentsTab then
         ClassTalentFrame.TalentsTab.LoadoutDropDown:SetRightClickCallback(nil);
     end
+    if self.linkButton then self.linkButton:Hide(); end
 end
 
 function Module:GetDescription()
-    return 'Adds a right-click option to the loadout dropdown to export your build.';
+    return [[
+Adds a right-click option to the loadout dropdown to export your build.
+
+Adds a button to link the currently shown build in chat.
+]];
 end
 
 function Module:GetName()
     return 'Export Loadouts';
 end
 
+function Module:GetOptions(defaultOptionsTable, db)
+    self.db = db;
+    local defaults = {
+        exportOnDropdownRightClick = true,
+        showLinkInChatButton = true,
+    };
+    for k, v in pairs(defaults) do
+        if db[k] == nil then
+            db[k] = v;
+        end
+    end
+
+    local counter = CreateCounter(10);
+
+    local get = function(info)
+        return self.db[info[#info]];
+    end
+    local set = function(info, value)
+        self.db[info[#info]] = value;
+        self:OnDisable();
+        self:OnEnable();
+    end
+    defaultOptionsTable.args.exportOnDropdownRightClick = {
+        type = 'toggle',
+        name = 'Export on Right-Click',
+        desc = 'Adds a right-click option to the loadout dropdown to export your build.',
+        order = counter(),
+        get = get,
+        set = set,
+    };
+    defaultOptionsTable.args.showLinkInChatButton = {
+        type = 'toggle',
+        name = 'Show '.. (TALENT_FRAME_DROP_DOWN_EXPORT_CHAT_LINK or 'Post in Chat') ..' Button',
+        desc = 'Adds a button to link the currently shown build in chat.',
+        order = counter(),
+        get = get,
+        set = set,
+    }
+
+    return defaultOptionsTable;
+end
+
+
 function Module:SetupHook()
     local talentsTab = ClassTalentFrame.TalentsTab;
 
+    if self.db.exportOnDropdownRightClick then
+        self:SetupDropdownHook(talentsTab);
+    end
+
+    if self.db.showLinkInChatButton then
+        if not self.linkButton then
+            self.linkButton = self:MakeLinkButton(talentsTab);
+        end
+        self.linkButton:Show();
+    end
+end
+
+function Module:MakeLinkButton(talentsTab)
+    local button = CreateFrame('Button', nil, talentsTab, 'UIPanelButtonNoTooltipTemplate, UIButtonTemplate');
+    button:SetText(TALENT_FRAME_DROP_DOWN_EXPORT_CHAT_LINK or 'Post in Chat');
+    button:SetSize(100, 22);
+    button:SetPoint('BOTTOMLEFT', 47, 5);
+    button:SetScript('OnClick', function()
+        local exportString = Util:GetLoadoutExportString(talentsTab);
+
+        if not TALENT_BUILD_CHAT_LINK_TEXT then
+            if not ChatEdit_InsertLink(exportString) then
+                ChatFrame_OpenChat(exportString);
+            end
+            return;
+        end
+
+        local specName = talentsTab:GetSpecName();
+        local className = talentsTab:GetClassName()
+        local specID = talentsTab:GetSpecID();
+        local classColor = RAID_CLASS_COLORS[select(2, GetClassInfo(talentsTab:GetClassID()))];
+        local level = LEVEL_CAP;
+
+        local linkDisplayText = ("[%s]"):format(TALENT_BUILD_CHAT_LINK_TEXT:format(specName, className));
+        local linkText = LinkUtil.FormatLink("talentbuild", linkDisplayText, specID, level, exportString);
+        local chatLink = classColor:WrapTextInColorCode(linkText);
+        if not ChatEdit_InsertLink(chatLink) then
+            ChatFrame_OpenChat(chatLink);
+        end
+    end);
+
+    return button;
+end
+
+function Module:SetupDropdownHook(talentsTab)
     local dropdown = talentsTab.LoadoutDropDown;
     dropdown:SetRightClickCallback(function(configID)
         local ok, configInfo = pcall(C_Traits.GetConfigInfo, configID);
