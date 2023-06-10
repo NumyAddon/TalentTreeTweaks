bc9615daea4f567bf637be17860a5333b2483bfa
Mark W <numedain@gmail.com>
2023-06-10 21:02:02 +0200

    Added extra number to the gates, to show how many points could be safely removed (closes #25)

--- modules/alwaysShowGates.lua
+++ modules/alwaysShowGates.lua
@@ -7,7 +7,9 @@ local L = TTT.L;
 
 local Module = Main:NewModule('AlwaysShowGates', 'AceHook-3.0');
 --- @type LibTalentTree
-local LTT = LibStub('LibTalentTree-1.0');
+local LTT = Util.LibTalentTree;
+local GATE_TEXT_FORMAT = '%d (+%d)';
+local TOOLTIP_FORMAT = L['%d points spent past the gate.\n%d extra points above the gate are free to be moved away.'];
 
 function Module:OnInitialize()
     self.gateInfo = {};
@@ -39,8 +41,16 @@ function Module:SetupHook()
     -- We have to create our own gatePool, because otherwise we will cause massive taint issues
     -- when acquiring a gate from the pool. Using our own pool causes no such issues
     self.gatePool = self.gatePool or CreateFramePool("FRAME", talentFrame.ButtonsParent, "TalentFrameGateTemplate");
-    self:SecureHook(talentFrame, 'RefreshGates');
+    self:SecureHook(talentFrame, 'RefreshGates', function() RunNextFrame(function() self:RefreshGates() end) end);
     self:RefreshGates();
+end
+
+local function GateOnEnter(gate)
+    if not gate or not gate.tooltip then return; end
+    local tooltip = GameTooltip;
+    tooltip:SetOwner(gate, "ANCHOR_LEFT", 4, -4);
+    tooltip:AddLine(gate.tooltip);
+    tooltip:Show();
 end
 
 function Module:RefreshGates()
@@ -56,11 +66,13 @@ function Module:RefreshGates()
         local condInfo = talentFrame:GetAndCacheCondInfo(gateInfo.conditionID);
         if firstButton and firstButton:IsVisible() and condInfo.isMet then
             local gate = self.gatePool:Acquire();
+            gate:SetScript('OnEnter', GateOnEnter);
             condInfo = self:EnrichConditionInfo(condInfo);
             gate:Init(talentFrame, firstButton, condInfo);
             talentFrame:AnchorGate(gate, firstButton);
             gate:Show();
             gate:SetAlpha(0.4);
+            gate.tooltip = condInfo.tooltip;
 
             talentFrame:OnGateDisplayed(gate, firstButton, condInfo);
         end
@@ -78,12 +90,84 @@ function Module:EnrichConditionInfo(condInfo)
             local spentAmountRequired = gateInfo.spentAmountRequired;
             local currencyID = gateInfo.traitCurrencyID;
             local spent = talentFrame.treeCurrencyInfoMap[currencyID].spent;
+            local extraSpentAboveGate = (0 - spentAmountRequired) + self:GetCurrencySpentAboveGate(condInfo.condID, currencyID);
+            local extraSpent = spentAmountRequired - spent;
             condInfo = Mixin({}, condInfo);
-            condInfo.spentAmountRequired = spentAmountRequired - spent;
+            condInfo.spentAmountRequired = GATE_TEXT_FORMAT:format(extraSpent, extraSpentAboveGate);
+            condInfo.tooltip = TOOLTIP_FORMAT:format(0 - extraSpent, extraSpentAboveGate);
 
             return condInfo;
         end
     end
 
     return condInfo;
+end
+
+function Module:GetCurrencySpentAboveGate(condID, currencyID)
+    local talentFrame = ClassTalentFrame.TalentsTab;
+    local nodesAboveGate = self:GetNodesAboveGate(condID, currencyID);
+    local spent = 0;
+    for _, nodeID in ipairs(nodesAboveGate) do
+        local nodeInfo = talentFrame:GetAndCacheNodeInfo(nodeID);
+        if nodeInfo and nodeInfo.ranksPurchased then
+            spent = spent + nodeInfo.ranksPurchased;
+        end
+    end
+
+    return spent;
+end
+
+local nodesCache = {};
+function Module:GetNodes()
+    local talentFrame = ClassTalentFrame.TalentsTab;
+    local treeID = talentFrame:GetTalentTreeID();
+    if not nodesCache[treeID] then
+        nodesCache[treeID] = C_Traits.GetTreeNodes(treeID);
+    end
+
+    return nodesCache[treeID];
+end
+
+local nodesAboveGateCache = {};
+function Module:GetNodesAboveGate(condID, currencyID)
+    if not nodesAboveGateCache[condID] then
+        local talentFrame = ClassTalentFrame.TalentsTab;
+        local treeID = talentFrame:GetTalentTreeID();
+        local nodes = self:GetNodes();
+        local nodesAboveGate = {};
+        local currencyIsClassCurrency = self:IsCurrencyClassCurrency(currencyID);
+        for _, nodeID in ipairs(nodes) do
+            local nodeInfo = talentFrame:GetAndCacheNodeInfo(nodeID);
+            if nodeInfo and (LTT:IsClassNode(treeID, nodeID) == currencyIsClassCurrency) then
+                local conditionFound = false;
+                for _, conditionID in ipairs(nodeInfo.conditionIDs) do
+                    if conditionID == condID then
+                        conditionFound = true;
+                        break;
+                    end
+                end
+                if not conditionFound then
+                    table.insert(nodesAboveGate, nodeID);
+                end
+            end
+        end
+
+        nodesAboveGateCache[condID] = nodesAboveGate;
+    end
+
+    return nodesAboveGateCache[condID];
+end
+
+local currencyCache = {};
+function Module:IsCurrencyClassCurrency(currencyID)
+    if currencyCache[currencyID] == nil then
+        local talentFrame = ClassTalentFrame.TalentsTab;
+        local configID = talentFrame:GetConfigID();
+        local treeID = talentFrame:GetTalentTreeID();
+        for i, currencyInfo in ipairs(C_Traits.GetTreeCurrencyInfo(configID, treeID, true)) do
+            currencyCache[currencyInfo.traitCurrencyID] = i == 1;
+        end
+    end
+
+    return currencyCache[currencyID];
 end
