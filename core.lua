c3dfd2d04c254d8a0f48e919f506ee99889f7834
Mark W <numedain@gmail.com>
2026-08-09 13:24:39 +0200

    Standardized the localisation system

--- core.lua
+++ core.lua
@@ -11,7 +11,6 @@ if not _G.TTT then _G.TTT = ns; end
 local Main = LibStub('AceAddon-3.0'):NewAddon(name, 'AceConsole-3.0', 'AceHook-3.0', 'NumyAceEvent-3.0');
 if not Main then return; end
 ns.Main = Main;
-ns.L = LibStub('AceLocale-3.0'):GetLocale(name);
 
 function Main:OnInitialize()
     if NumyFunctionProfiler then
